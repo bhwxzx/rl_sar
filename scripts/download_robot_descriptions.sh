9b78cfc59f23644da2ba29d3431794bde2fe3a70
lfr <12926148+jiandangbahuang@user.noreply.gitee.com>
2025-12-12 19:25:50 +0800

    修改description的git网址，改为自己fork的仓库

--- scripts/download_robot_descriptions.sh
+++ scripts/download_robot_descriptions.sh
@@ -28,7 +28,8 @@ fi
 ROBOT_DESC_DIR="${PROJECT_ROOT}/${TARGET_DIR}"
 
 # Repository configuration
-REPO_URL="https://github.com/fan-ziqi/rl_sar_zoo.git"
+# REPO_URL="https://github.com/fan-ziqi/rl_sar_zoo.git"
+REPO_URL="https://github.com/bhwxzx/rl_sar_zoo.git"
 REPO_BRANCH="main"
 
 # Expected version - update this when URDF files need to be updated
