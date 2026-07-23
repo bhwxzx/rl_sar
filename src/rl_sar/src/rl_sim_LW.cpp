ec7109f9897d32dee86a0d3a22e43684d56ccbd2
lfr <12926148+jiandangbahuang@user.noreply.gitee.com>
2026-07-23 11:09:46 +0800

    修改踝关节KP、KD，修改速度阈值

--- src/rl_sar/src/rl_sim_LW.cpp
+++ src/rl_sar/src/rl_sim_LW.cpp
@@ -517,7 +517,7 @@ void RL_Real::RunModel()
             this->gait_phase_time -= 1.0f; 
         }
 
-        float vel_threshold = 0.05f;
+        float vel_threshold = 0.1f;
         float cmd_norm = std::sqrt(this->control.x * this->control.x + 
                                    this->control.y * this->control.y + 
                                    this->control.yaw * this->control.yaw);
